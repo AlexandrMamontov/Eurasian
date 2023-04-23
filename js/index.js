8b78e61ffeb23dafe9844967ac49022996f8c06e
alexandr_mamontov <sasham13101999@yandex.ru>
2023-04-23 03:03:22 +0300

    change index and media

--- js/index.js
+++ js/index.js
@@ -12,3 +12,23 @@ document.addEventListener('DOMContentLoaded', (e) => {
     e.preventDefault()
   })
 })
+
+
+let burger = document.querySelector('.header__burger');
+let menu = document.querySelector('.header__nav');
+let menuLinks = menu.querySelectorAll('.nav__link')
+
+burger.addEventListener('click',
+  function () {
+    burger.classList.toggle('header__burger--active');
+    menu.classList.toggle('header__nav--active');
+    document.body.classList.toggle('stop-scroll');
+})
+
+menuLinks.forEach(function (el) {
+  el.addEventListener('click', function () {
+    burger.classList.remove('header__burger--active');
+    menu.classList.remove('header__nav--active');
+    document.body.classList.remove('stop-scroll');
+  })
+})
